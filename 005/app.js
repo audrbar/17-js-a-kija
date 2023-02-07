681cff1bb2966ee99b82d4669f4d5fa5ea47f5b4
audrbar <102819309+audrbar@users.noreply.github.com>
2023-02-07 14:19:45 +0200

    objektai

--- 005/app.js
+++ 005/app.js
@@ -6,4 +6,50 @@ localStorage.setItem('mayFavoriteColor', color);
 console.log(localStorage.getItem('mayFavoriteColor'));
 
 
+console.log('--------- Klases Objektai ---------');
+// 1 sukuriamas objekto planas - klase
+// kuriam katino objekto plana - construktorius, kiti metodai
 
+class sayName {
+    static voice = 'big';
+    static whatIsYourVoice() {
+        return this.voice;
+    }
+    constructor() {
+        this.papers = 'yes';
+    }
+    whatIsYourName() {
+        return this.name;
+    }
+}
+
+class Cat extends sayName {
+    constructor(name, color) {
+        super();
+        this.name = name;
+        this.color = color;
+        this.age = parseInt(Math.random() * 10 + 1);
+    }
+    miauMethod() {
+        return 'miau';
+    }
+}
+
+class Dog extends sayName {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.age = parseInt(Math.random() * 15 + 1);
+    }
+}
+const murka = new Cat('murka', 'brown');
+const pilkis = new Cat('pilkis', 'gray');
+const reksas = new Dog('reksas');
+// kas yra murka?
+// murka.name = 'Murka';
+console.log(murka.whatIsYourName());
+console.log(pilkis.whatIsYourName());
+console.log(murka);
+console.log(reksas);
+console.log(Cat);
+console.log(sayName.voice);
