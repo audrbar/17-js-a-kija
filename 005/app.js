3a2d3b716c0c6e27d4410b6b36591ec2e2dcaf25
audrbar <102819309+audrbar@users.noreply.github.com>
2023-02-07 13:03:59 +0200

    localStorage

---
+++ 005/app.js
@@ -0,0 +1,9 @@
+console.log('--------- Local Storage ---------');
+// narsykles savybe
+let color = 'skyblue';
+console.log(color);
+localStorage.setItem('mayFavoriteColor', color);
+console.log(localStorage.getItem('mayFavoriteColor'));
+
+
+
