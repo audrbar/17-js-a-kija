d5de561a30a756158649eb593e9e00cb7341a6b7
audrbar <audrbar@gmail.com>
2023-02-10 19:00:15 +0200

    props

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.scss';
 import Animal from './components/jb/Animal';
 
+const obj = ['racoon', 'fox'];
+
 function App() {
   // const fox = <u>Fox No: {rand(100, 150)}</u>;
   // const laba = <i>Labas</i>;
@@ -8,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+      <h1>{obj}</h1>
+
       <Animal animalName="Racoon" color="coral" h1Class="blue" />
       <Animal animalName="Fox"color="red" h1Class="green" />
       <Animal animalName="Rabbit"color="black" h1Class="yellow" />
