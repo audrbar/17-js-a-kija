806c0706bda92b7cfb7d27fb40c47d280e1fe95c
audrbar <audrbar@gmail.com>
2023-02-10 16:58:34 +0200

    006

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,14 +1,33 @@
 import './App.css';
-import Racoon from './components/006/Racoon';
-import Cat from './components/006/Cat';
 
 function App() {
+
+  const rand = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  // const fox = <u>Fox No: {rand(100, 150)}</u>;
+  // const laba = <i>Labas</i>;
+  const font = 'monospace';
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>labas</h1>
-        <Racoon />
-        <Cat />
+        <h1 style={{
+          color: 'crimson',
+          backgroundColor: 'skyblue',
+          padding: '8px 30px',
+          fontSize: rand(20, 50) + 'px',
+          letterSpacing: rand(0, 1) ? '5px' : 0,
+          fontFamily: font
+        }}>Racoon No: {
+
+          rand(11, 15)
+          
+        }</h1>
+
       </header>
     </div>
   );
---
+++ firstapp/src/components/006/006/006.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Racoon from '../Racoon';
+import Cat from '../Cat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>labas</h1>
+        <Racoon />
+        <Cat />
+      </header>
+    </div>
+  );
+}
+
+export default App;
