1bb45a7f62c8066f25466661947a567fadda5e50
audrbar <102819309+audrbar@users.noreply.github.com>
2023-02-21 12:24:41 +0200

    inputs

---
+++ firstapp/src/012.jsx
@@ -0,0 +1,46 @@
+'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import './App.scss';
+import BaseButton from './components/011/Design/BaseButton';
+import BlueButton from './components/011/Design/BlueButton';
+import RedButton from './components/011/Design/RedButton';
+import Sq from './components/011/Design/Sq';
+import { withAdd, withClear, withColor } from './components/011/HOCs/sq';
+// import Circle from './components/011/Circle';
+// import Number from './components/011/Number';
+
+function App() {
+
+    const [sq, setSq] = useState([]);
+
+    const BaseButtonWithAdd = withAdd(BaseButton);
+    const RedButtonWithClear = withClear(RedButton);
+    const BlueButtonWithColor = withColor(BlueButton);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                {/* <Circle>
+                    <Number type="n1" />
+                    <Number type="n2" />
+                    <Number type="n3" />
+                    <Number type="n4" />
+                </Circle>  */}
+                <div className="sq-bin">
+                    {
+                        sq.map((s, i) => s.show ? <Sq key={i} s={s} i={i} setSq={setSq} /> : null)
+                    }
+                </div>
+                <div className="sq-bin">
+                    <BaseButtonWithAdd title="add" setSq={setSq} />
+                    <RedButtonWithClear title="clear" setSq={setSq} />
+                    <BlueButtonWithColor title="color" setSq={setSq} /> 
+                </div>
+            </header>
+        </div>
+    );
+
+}
+
+export default App;
