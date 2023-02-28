0dd572ef0e8436cee02a035aaa05a269fc73480b
audrbar <audrbar@gmail.com>
2023-02-28 20:23:19 +0200

    server side rendering

--- firstserver/app.js
+++ firstserver/app.js
@@ -2,16 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
+const meniu = `
+<a href="/">home</a>
+<a href="/racoon">racoon</a>
+<a href="/fox">fox</a>
+<a href="/volf">volf</a>
+`;
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`
+  ${meniu}
+  <h1>Hello World!</h1>`);
 });
 
 app.get("/racoon", (req, res) => {
-  res.send("Hello Racoon!");
+  res.send(`
+    ${meniu}
+    <h1>Hello Racoon!</h1>`);
 });
 
 app.get("/Fox", (req, res) => {
-  res.send("Hello Fox!");
+  res.send(`
+    ${meniu}
+    <h1>Hello Fox!</h1>`);
+});
+
+app.get("/Volf", (req, res) => {
+  res.send(`
+    ${meniu}
+    <h1>Hello Volf!</h1>`);
 });
 
 app.listen(port, () => {
