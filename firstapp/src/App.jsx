5947695ce6c6cd49c2d83e4ba434bfe1a6554af1
audrbar <102819309+audrbar@users.noreply.github.com>
2023-03-01 15:02:09 +0200

    dices-server

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from 'react';
 import Create from './components/DicesServer/Create';
 import List from './components/DicesServer/List';
-import { create, destroy, edit, read } from './components/DicesServer/localStorage';
-import Messages from './components/DicesServer/Messages';
 import './components/DicesServer/style.scss';
-import {v4 as uuidv4} from 'uuid';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 const URL = 'http://localhost:3003/dices';
-
-const KEY = 'FancyDices';
 
 function App() {
 
@@ -22,48 +18,71 @@ function App() {
     const [editData, setEditData] = useState(null);
     const [messages, setMessages] = useState(null);
 
+
     useEffect(() => {
         axios.get(URL)
-        .then(res => {
-            setList(res.data);
-        })
-    }, [])
+            .then(res => {
+                setList(res.data);
+            });
+    }, [lastUpdate]);
+
 
 
     useEffect(() => {
         if (null === createData) {
             return;
         }
-        axios.post(URL, createData)
-        .then(res => {
-            console.log(res.data);
-        })
+        // pazadas
+        const promiseId = uuidv4();
+        setList(d => [...d, { ...createData, promiseId }]);
+
+        // serveris
+        axios.post(URL, { ...createData, promiseId })
+            .then(res => {
+                setList(d => d.map(d => res.data.promiseId === d.promiseId ? { ...d, id: res.data.id, promiseId: null } : { ...d }));
+                console.log(res.data);
+            });
+
     }, [createData]);
 
-    
+
+    useEffect(() => {
+        if (null === deleteData) {
+            return;
+        }
+        axios.delete(URL + '/' + deleteData.id)
+            .then(res => {
+                console.log(res.data);
+                setLastUpdate(Date.now());
+            });
+
+    }, [deleteData]);
+
+
+
     return (
         <>
-        <div className="dices">
-            <div className="content">
-                <div className="left">
-                    <Create setCreateData={setCreateData}/>
-                </div>
-                <div className="right">
-                    <List 
-                    list={list}
-                    setDeleteModal={setDeleteModal}
-                    deleteModal={deleteModal}
-                    setDeleteData={setDeleteData}
-                    editModal={editModal}
-                    setEditModal={setEditModal}
-                    setEditData={setEditData}
-                     />
+            <div className="dices">
+                <div className="content">
+                    <div className="left">
+                        <Create setCreateData={setCreateData} />
+                    </div>
+                    <div className="right">
+                        <List
+                            list={list}
+                            setDeleteModal={setDeleteModal}
+                            deleteModal={deleteModal}
+                            setDeleteData={setDeleteData}
+                            editModal={editModal}
+                            setEditModal={setEditModal}
+                            setEditData={setEditData}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-        {
-            messages && <Messages messages={messages} />
-        }
+            {
+                // messages && <Messages messages={messages} />
+            }
         </>
     );
 
--- firstserver/app.js
+++ firstserver/app.js
@@ -1,42 +1,67 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 3003;
+
 app.use(cors());
 
 app.use(
-  express.urlencoded({
-      extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 app.use(express.json());
 
 
+
 // API
-
 app.get('/dices', (req, res) => {
-  let allData = fs.readFileSync('./data.json', 'utf8');
-  allData = JSON.parse(allData);
-  res.json(allData);
+    let allData = fs.readFileSync('./data.json', 'utf8');
+    allData = JSON.parse(allData);
+    res.json(allData);
 });
 
-app.post("/dices", (req, res) => {
-  let allData = fs.readFileSync('./data.json', 'utf8');
-  allData = JSON.parse(allData);
-  const data = {...req.body};
-  data.id = uuidv4();
-  allData.push(data);
-  allData = JSON.stringify(allData);
-  fs.writeFileSync('./data.json', allData, 'utf8')
-  
-  res.json({ message: 'OK' });
+app.post('/dices', (req, res) => {
+    let allData = fs.readFileSync('./data.json', 'utf8');
+    allData = JSON.parse(allData);
+    const id = uuidv4();
+    const data = {
+        number: req.body.number,
+        size: req.body.size,
+        color: req.body.color,
+        id
+    };
+    promiseId = req.body.promiseId
+    allData.push(data);
+    allData = JSON.stringify(allData);
+    fs.writeFileSync('./data.json', allData, 'utf8');
+
+    res.json({
+        message: 'OK',
+        promiseId,
+        id
+    });
 });
+
+
+app.delete('/dices/:id', (req, res) => {
+    let allData = fs.readFileSync('./data.json', 'utf8');
+    allData = JSON.parse(allData);
+    let deletedData = allData.filter(d => req.params.id !== d.id);
+    deletedData = JSON.stringify(deletedData);
+    fs.writeFileSync('./data.json', deletedData, 'utf8');
+
+    res.json({ message: 'OK' });
+});
+
+
+
 
 
 
 app.listen(port, () => {
-  console.log(`Dices is on port number: ${port}`);
+    console.log(`Dices is on port number: ${port}`);
 });
